a58fa483918761e8c338dfec74fb8a446fa7f80a
emichulo <albu.emanuel26@gmail.com>
2026-04-08 13:23:52 +0300

    core logic and imput verification implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,38 @@
+#include <cstdlib>
+#include <filesystem>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
-int main() {}
+namespace fs = std::filesystem;
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    std::cerr << "Correct usage: " << argv[0] << " <argv1> <argv2>\n";
+    return EXIT_FAILURE;
+  }
+  try {
+    fs::path source = argv[1];
+    std::string output = argv[2];
+    // check if the file exist to system
+    if (!fs::exists(source)) {
+      throw std::runtime_error("Source file doesn't exist: " + source.string());
+    }
+    // check the extension
+    if (source.extension() != ".cpp") {
+      throw std::runtime_error("This: " + source.string() + " is not a cpp file!");
+    }
+
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+
+  std::string command = "g++ ";
+  command.append(argv[1]);
+  command.append(" -o ");
+  command.append(argv[2]);
+
+  std::cout << argv[1];
+  std::system(command.c_str());
+}
